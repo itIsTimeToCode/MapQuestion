e7da05c013bea7fa29cfbb51699d97884ae4722f
PC <96895234+itIsTimeToCode@users.noreply.github.com>
2026-04-21 23:16:57 -0700

    SORA IS NOT A CHINESE MAN
    Implement a method to map strings to their second occurrence index.

---
+++ SecondOccurrenceMapper.java
@@ -0,0 +1,29 @@
+/**
+ * TODO:
+ * Implement this method to return a map where:
+ * - Each key is a string from the list that appears at least twice.
+ * - Each value is the index of the SECOND occurrence of that string.
+ * - Only include each string once in the map (i.e., ignore further duplicates).
+ *
+ * Example 1:
+ * Input: ["a", "b", "a", "c", "b"]
+ * Output: {"a"=2, "b"=4}
+ *
+ * Example 2:
+ * Input: ["x", "y", "z"]
+ * Output: {}
+ *
+ * Example 3:
+ * Input: ["cat", "dog", "cat", "cat", "dog"]
+ * Output: {"cat"=2, "dog"=4}
+ *
+ * Notes:
+ * - The "second occurrence" means the index where the element appears for the second time.
+ * - Indexing is 0-based.
+ * - Do not include elements that appear only once.
+ */
+public Map<String, Integer> mapSecondOccurrenceIndex(List<String> list) {
+    
+    // TODO: write your implementation here
+    
+}
